975f9b75d28dfa37d54dfe0b5d257cff467f9c76
AlexF <fennecfr@gmail.com>
2024-06-18 17:22:20 +0200

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
- //Aquí tu código
+    
+  const [name, setName] = useState("Sofía");
+  const [newName, setNewName] = useState('');
+
+  return (
+    <div className="App">
+      <h2>Teacher Name: {name}</h2>
+      <ul>
+        <li onClick={() => setName("Data")}>Data</li>
+        <li onClick={() => setName("Reyes")}>Reyes</li>
+        <li onClick={() => setName("Yolanda")}>Yolanda</li>
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
